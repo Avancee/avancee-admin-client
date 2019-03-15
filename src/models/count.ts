60ef3c6a9e2b44e45dd43f7475a18bc15dc58b01
Edward <3zangelks@gmail.com>
2019-03-15 23:58:56 +0800

    chore: import rematch

---
+++ src/models/count.ts
@@ -0,0 +1,19 @@
+
+import { createModel } from '@rematch/core'
+
+export const delay = (ms: number) =>
+    new Promise(resolve => setTimeout(resolve, ms))
+export type DolphinsState = number
+
+export const dolphins = createModel({
+	state: 0,
+	reducers: {
+		increment: (state: DolphinsState) => state + 1,
+	},
+	effects: dispatch => ({
+		async incrementAsync() {
+			await delay(500)
+			dispatch.dolphins.increment()
+		},
+	}),
+})
