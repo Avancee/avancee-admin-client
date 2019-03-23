89d07a1296254c96b604d0eda21face52817c60e
Edward <3zangelks@gmail.com>
2019-03-23 23:14:53 +0800

    fix(#33690): https://github.com/DefinitelyTyped/DefinitelyTyped/issues/33690

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import * as styles from './index.m.css';
+import {connect} from 'react-redux';
 import * as avatar from '../../assets/avatar.jpeg';
 import './home.css';
 
@@ -9,8 +10,17 @@ const Home: React.FunctionComponent = () => {
     <div>
       <h1 className={styles.text}>Hello World!</h1>
       <img src={avatar} className="avatar" />
+
     </div>
   );
 };
+const mapState = state => ({
+count: state.count,
+});
 
-export default Home;
+const mapDispatch = ({ count: { addBy, addByAsync }}) => ({
+  addByOne: () => addBy(1),
+  addByOneAsync: () => addByAsync(1)
+});
+
+export default connect(mapState, mapDispatch)(Home);
